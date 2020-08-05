96e42af2d2a35dcefa678248bf0720b3ccef4a6b
neurus <mrneurus@gmail.com>
2020-08-05 16:22:56 -0300

    toma dark

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const btnVer = document.getElementById('btn-ver')
-const btnMode = document.getElementById('mode')
+const btnMode = document.getElementById('mode') 
 const btnInsertarLink = document.getElementById('btn-insertar')
 const btnNegrita = document.getElementById('btn-negrita')
 
@@ -8,7 +8,7 @@ document.onload = cambiarbg()
 btnInsertarLink.addEventListener('click',insertarLink)
 btnNegrita.addEventListener('click',pasarNegrita)
 btnVer.addEventListener('click',cambiarBorde)
-btnMode.addEventListener('click',cambiarModo)
+btnMode.addEventListener('click',cambiarModo) 
 
 
 
@@ -30,10 +30,48 @@ function pasarNegrita (e){
 
 
 function cambiarModo(e){
-    e.preventDefault()
-    let bodysel = document.querySelector('body')
-    bodysel.style.backgroundColor='#ffff' 
-    console.log(bodysel)
+    e.preventDefault() 
+    let mainsel = document.querySelector('main')
+    let container=document.querySelector('#contenedor')
+    mainsel.style.backgroundColor='#ffff'  
+    
+
+    
+
+    let flagInicial=btnMode.classList.contains('dark')
+    
+        if(flagInicial){
+            btnMode.classList.remove('dark')
+            btnMode.classList.add('luz')
+            container.style.setProperty('--border','rgb(34, 36, 41) solid');
+            container.style.setProperty('--text-color','#000000')
+
+        }else{
+            btnMode.classList.remove('luz')
+            btnMode.classList.add('dark')
+            mainsel.style.backgroundColor='rgb(34, 36, 41)' 
+            container.style.setProperty('--border','solid 2px #fff');
+            container.style.setProperty('--text-color','#ffff')
+           
+            
+        }
+
+       
+   
+
+
+
+
+    /* otra forma de hacerlo con un for, por si lo que necesitamos no esta en un contenedor o no son familia */
+   /*  let textoscolor= document.getElementsByClassName('text-color')
+    for (var i = 0; i < textoscolor.length; i++) {
+            textoscolor[i].style.setProperty('--text-color','#00000'); 
+        } */
+
+    
+   
+    
+    
 }
 
 
@@ -54,13 +92,9 @@ function insertarLink(e){
 
 
 function cambiarbg(){
-
     setTimeout(() => {
        
          document.querySelector("#imagen").style.backgroundColor='rgb(65, 199, 199)'; 
-        
-         
-
       }, 3000);
 
 
